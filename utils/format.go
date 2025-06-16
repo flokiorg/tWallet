1625682f4be67b3f799164031a476293fd6c289f
mechion <4387895+mechion@users.noreply.github.com>
2025-06-16 14:38:42 +0000

    Add fee helper

--- utils/format.go
+++ utils/format.go
@@ -4,11 +4,29 @@
 
 package utils
 
-import "strconv"
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/flokiorg/go-flokicoin/chainutil"
+)
+
+type FeeOption struct {
+	Label  string
+	Amount chainutil.Amount
+}
 
 func ParseIntWithDefault(value string, defaultValue int) (int, error) {
 	if value == "" {
 		return defaultValue, nil
 	}
 	return strconv.Atoi(value)
+}
+
+func BuildFeesOptions(slow, medium, fast float64) []FeeOption {
+	return []FeeOption{
+		{Label: fmt.Sprintf(" Slow: %.0f loki/vB ", slow), Amount: chainutil.Amount(slow)},
+		{Label: fmt.Sprintf(" Medium: %.0f loki/vB ", medium), Amount: chainutil.Amount(medium)},
+		{Label: fmt.Sprintf(" Fast: %.0f loki/vB ", fast), Amount: chainutil.Amount(fast)},
+	}
 }
